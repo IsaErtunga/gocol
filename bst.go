ac917ce22b847ecda44ea7948f548df2e1e31d7a
IsaErtunga <isa.ertunga@gmail.com>
2023-08-12 00:15:26 +0200

    added binary search tree, not finished

---
+++ bst.go
@@ -0,0 +1,91 @@
+package gocol
+
+import (
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
+
+type BST[K constraints.Ordered, V any] struct {
+	root  *BST[K, V]
+	left  *BST[K, V]
+	right *BST[K, V]
+	key   K
+	val   V
+}
+
+func NewBST[K constraints.Ordered, V any]() *BST[K, V] {
+	var key K
+	var val V
+	return &BST[K, V]{
+		root:  nil,
+		left:  nil,
+		right: nil,
+		key:   key,
+		val:   val,
+	}
+}
+
+func (bst *BST[K, V]) Insert(key K, val V) {
+	var travPtr *BST[K, V] = bst.root
+	var rootPtr *BST[K, V] = nil
+	leaf := &BST[K, V]{
+		root:  nil,
+		left:  nil,
+		right: nil,
+		key:   key,
+		val:   val,
+	}
+
+	// Traverse tree to correct leaf
+	for travPtr != nil {
+		rootPtr = travPtr
+		if leaf.key < travPtr.key {
+			travPtr = travPtr.left
+		} else {
+			travPtr = travPtr.right
+		}
+	}
+	leaf.root = rootPtr
+
+	// Attach new leaf node
+	if rootPtr == nil {
+		// Empty tree case
+		bst.root = leaf
+	} else if leaf.key < rootPtr.key {
+		rootPtr.left = leaf
+	} else {
+		rootPtr.right = leaf
+	}
+}
+
+func (bst *BST[K, V]) Min() *BST[K, V] {
+	travPtr := bst.root
+	for travPtr.left != nil {
+		travPtr = travPtr.left
+	}
+	return travPtr
+}
+
+func (bst *BST[K, V]) Max() *BST[K, V] {
+	travPtr := bst.root
+	for travPtr.right != nil {
+		travPtr = travPtr.right
+	}
+	return travPtr
+}
+
+func (bst *BST[K, V]) Search() {
+}
+
+func (bst *BST[K, V]) Print() {
+	inorderWalk[K, V](bst.root)
+}
+
+func inorderWalk[K constraints.Ordered, V any](t *BST[K, V]) {
+	if t != nil {
+		inorderWalk[K, V](t.left)
+		fmt.Print(t.key, " ")
+		inorderWalk[K, V](t.right)
+	}
+}
---
+++ bst_test.go
@@ -0,0 +1,22 @@
+package gocol
+
+import (
+	"testing"
+)
+
+func TestBSTInsert(t *testing.T) {
+	bst := NewBST[int, string]()
+	bst.Insert(5, "a")
+	bst.Insert(7, "b")
+	bst.Insert(3, "c")
+	bst.Insert(6, "d")
+	bst.Insert(2, "e")
+
+	if min := bst.Min(); min.key != 2 {
+		t.Fail()
+	}
+	if max := bst.Max(); max.key != 7 {
+		t.Fail()
+	}
+
+}
